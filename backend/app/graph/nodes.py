4bcad5888064cf3d8bb48234499c54c61fc632b8
raulgpt <raulgooo@outlook.com>
2026-01-12 10:37:22 -0600

    I moved my graph folder out of pycache i accidentaly put it there lol

---
+++ backend/app/graph/nodes.py
@@ -0,0 +1,12 @@
+from langchain import 
+from backend.app.graph.models import ProcessState
+from backend.app import gemini_logic
+
+#First node, get the book file from google cloud storage and put it into the state
+async def get_book_node(state: ProcessState):
+    get_book_result = gemini_logic.get_book(book_name=state.book_name)
+    return {state.book_name: get_book_result}
+
+async def book_outline_node(state: ProcessState):
+    
+
